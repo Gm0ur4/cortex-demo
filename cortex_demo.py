b3a3c673073c58a955ac97e34f473dc9dc868de8
inteligenciacortex <97255018+Gm0ur4@users.noreply.github.com>
2026-02-12 14:29:15 -0300

    Update cortex_demo.py

--- cortex_demo.py
+++ cortex_demo.py
@@ -128,8 +128,8 @@ st.markdown("""
 
     .carousel-item-image-only {
         flex: 0 0 calc(33.333% - 27px);
-        min-width: 820px;
-        height: 430px;
+        min-width: 805px;
+        height: 415px;
         border-radius: 8px;
         overflow: hidden;
         border: 1px solid rgba(255, 255, 255, 0.1);
